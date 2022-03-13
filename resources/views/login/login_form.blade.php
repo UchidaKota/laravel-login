6ceefaee8353358f10e4db1922456ca618df1602
UchidaKota <gilchan0822@gmail.com>
2022-03-13 14:52:49 +0900

    ログインフォーム作成

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>ログインフォーム</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+</head>
+<body>
+    <form class="form-signin" method="POST" action="{{ route('login') }}">
+        @csrf
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address"  autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" >
+        <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+    </form>
+</body>
+</html>
